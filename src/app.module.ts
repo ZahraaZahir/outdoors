268f8509a687a020a44dd4bc4b93b6548a159860
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-05-31 23:46:21 +0300

    feat: set up validation pipe, create config file, and import config and prisma modules to the app level... :')

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service.js';
 import { AuthModule } from './auth/auth.module.js';
 import { ToursModule } from './tours/tours.module.js';
 import { BookingsModule } from './bookings/bookings.module.js';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module.js';
+import config from './config/config.js';
 
 @Module({
-  imports: [AuthModule, ToursModule, BookingsModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    PrismaModule,
+    AuthModule,
+    ToursModule,
+    BookingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
